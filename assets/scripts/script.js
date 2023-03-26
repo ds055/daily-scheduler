82b6a4e5c1b4a9498c9f8dc2145e0078603af2c9
Drew Smith <drgh056@gmail.com>
2023-03-26 00:22:36 -0500

    Add js to dynamically fill event description based on local data

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,9 +4,10 @@ const dateDisplay = $("#currentDay");
 const btns = $(".btn")
 const hourRows = $(".hour");
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+/* Wrap all code that interacts with the DOM in a call to jQuery to ensure that 
+the code isn't run until the browser has finished rendering all the elements
+in the html. */
+
 $(function () {
   init();
 
@@ -35,47 +36,39 @@ $(function () {
       }
     }
     
-  // load saved data; if none exists, create empty array
-  var workDaySchArr = JSON.parse(localStorage.getItem("workDaySchedule")) || [];
+    // load saved data; if none exists, create empty array
+    var workDaySchArr = JSON.parse(localStorage.getItem("workDaySchedule")) || [];
 
-  for (const event of workDaySchArr){
-    
-  }
-  
+    // loop through local data and set value in .description elements to match
+    for (const event of workDaySchArr) {
+      $('#hour-' + event.time).children(".description").val(event.event)
+    }
   }
 
 
   // event handler
   btns.on("click", saveSchedule);
 
-
   function saveSchedule(event) {
     //button clicked
     const btnClicked = event.target;
-
     //get parent time-block, get id, trim for just number
     const schHour = $(btnClicked).closest(".time-block").prop("id").slice(5);
-
     // get text from input area
     const schEvent = $(btnClicked).siblings(".description").val() || $(btnClicked).parent().siblings(".description").val();
-
     //get saved schedule
     var workDaySchArr = JSON.parse(localStorage.getItem("workDaySchedule")) || [];
-
-    // create new object with players inputted initials and scores
+    // create new object with time based on element id and "event" entered into input field
     var newEvent = {
         time: parseInt(schHour),
         event: schEvent
     };
-
     // if object for given time already exists, delete and make room for new time
     var workDaySchArr = $.grep(workDaySchArr, function(obj){
       return obj.time != newEvent.time;
     })
-
     // add new object to array
     workDaySchArr.push(newEvent);
-
     // place new array in local storage
     localStorage.setItem("workDaySchedule", JSON.stringify(workDaySchArr));
   }
